80165f55f7739a356840c91d4b921a84688e39f8
ranjitcd <ranjitcd5@gmail.com>
2025-10-24 18:57:33 +0530

    cors added

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // MongoDB Connection
